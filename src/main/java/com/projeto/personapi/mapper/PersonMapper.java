a28577e2e1f99880f7267c7415de39f5c6ef1eb0
Franciscoas4 <franciscoas4@gmail.com>
2021-08-05 23:06:43 -0300

    API de Person

---
+++ src/main/java/com/projeto/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.projeto.personapi.mapper;
+
+import com.projeto.personapi.dto.request.PersonDTO;
+import com.projeto.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
